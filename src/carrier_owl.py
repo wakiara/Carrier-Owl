6dbaeeac128d0131bb0c40c29558397e155ec158
wakiara <78192586+wakiara@users.noreply.github.com>
2022-09-07 18:10:57 +0900

    Update carrier_owl.py

--- src/carrier_owl.py
+++ src/carrier_owl.py
@@ -158,9 +158,10 @@ def get_translated_text(from_lang: str, to_lang: str, from_text: str, driver) ->
 
 def get_text_from_page_source(html: str) -> str:
     soup = BeautifulSoup(html, features='lxml')
+    
 #     search_class = "lmt__inner_textarea_container"
-#     search_class = "lmt__translations_as_text"
-    search_class = "lmt__translations_as_text__text_btn"
+    search_class = "lmt__translations_as_text"
+#     search_class = "lmt__translations_as_text__text_btn"
     
     print(soup.find(class_=search_class))
     target_elem = soup.find(class_=search_class)
